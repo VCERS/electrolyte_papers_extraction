ce8789d0cbe4bf68c0ce85e38f6be2b8b23debf1
breadbread1984 <breadbread1984@163.com>
2024-10-28 06:03:11 +0000

    update

--- knowledge_graph_llm/models.py
+++ knowledge_graph_llm/models.py
@@ -10,6 +10,67 @@ from langchain_community.llms import HuggingFaceEndpoint
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain_openai import ChatOpenAI
 from langchain.llms.base import LLM
+from langchain_core.utils.pydantic import is_basemodel_instance, is_basemodel_subclass
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.runnables import Runnable, RunnableLambda
+from langchain_core.runnables.passthrough import RunnablePassthrough
+from langchain_core.runnables.base import RunnableMap
+from operator import itemgetter
+
+class StructuredLLM(LLM):
+  def parse_response(self, message) -> str:
+    """Extract `function_call` from `AIMessage`."""
+    if isinstance(message, AIMessage):
+        kwargs = message.additional_kwargs
+        tool_calls = message.tool_calls
+        if len(tool_calls) > 0:
+            tool_call = tool_calls[-1]
+            args = tool_call.get("args")
+            return json.dumps(args)
+        elif "function_call" in kwargs:
+            if "arguments" in kwargs["function_call"]:
+                return kwargs["function_call"]["arguments"]
+            raise ValueError(
+                f"`arguments` missing from `function_call` within AIMessage: {message}"
+            )
+        else:
+            raise ValueError("`tool_calls` missing from AIMessage: {message}")
+    raise ValueError(f"`message` is not an instance of `AIMessage`: {message}")
+
+  def _is_pydantic_class(self, obj) -> bool:
+        return isinstance(obj, type) and (
+            is_basemodel_subclass(obj) or BaseModel in obj.__bases__
+        )
+  def with_structured_output(self, schema, *, include_raw = False, **kwargs):
+        if kwargs:
+            raise ValueError(f"Received unsupported arguments {kwargs}")
+        is_pydantic_schema = self._is_pydantic_class(schema)
+        if schema is None:
+            raise ValueError(
+                "schema must be specified when method is 'function_calling'. "
+                "Received None."
+            )
+        llm = self.bind(tools=[schema], format="json")
+        if is_pydantic_schema:
+            output_parser: OutputParserLike = PydanticOutputParser(  # type: ignore[type-var]
+                pydantic_object=schema  # type: ignore[arg-type]
+            )
+        else:
+            output_parser = JsonOutputParser()
+
+        parser_chain = RunnableLambda(self.parse_response) | output_parser
+        if include_raw:
+            parser_assign = RunnablePassthrough.assign(
+                parsed=itemgetter("raw") | parser_chain, parsing_error=lambda _: None
+            )
+            parser_none = RunnablePassthrough.assign(parsed=lambda _: None)
+            parser_with_fallback = parser_assign.with_fallbacks(
+                [parser_none], exception_key="parsing_error"
+            )
+            return RunnableMap(raw=llm) | parser_with_fallback
+        else:
+            return llm | parser_chain
+
 
 def Llama3(locally = False):
   login(token = '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')
@@ -46,7 +107,7 @@ def Llama3(locally = False):
 
 def Llama3_FA2(locally = False):
   assert locally == True, "must be locally!"
-  class LLama3FA2(LLM):
+  class LLama3FA2(StructuredLLM):
     tokenizer: AutoTokenizer = None
     model: AutoModelForCausalLM = None
     def __init__(self,):
@@ -102,7 +163,7 @@ def Qwen2(locally = False):
 
 def Qwen2_FA2(locally = False):
   assert locally == True, "must be locally!"
-  class Qwen2FA2(LLM):
+  class Qwen2FA2(StructuredLLM):
     tokenizer: AutoTokenizer = None
     model: AutoModelForCausalLM = None
     def __init__(self,):
